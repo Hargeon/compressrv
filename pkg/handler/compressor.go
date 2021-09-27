7385c43dc721d336df523fc24c010675bce4de45
Hargeon <hirami14590@gmail.com>
2021-09-27 19:30:44 +0300

    Add gRPC server

--- pkg/handler/compressor.go
+++ pkg/handler/compressor.go
@@ -2,16 +2,29 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"github.com/Hargeon/compressrv/pkg/proto"
+	"github.com/Hargeon/compressrv/pkg/service"
 )
 
-type CompressorHandler struct{}
+type CompressorHandler struct {
+	service *service.Service
+}
 
-func NewCompressorHandler() *CompressorHandler {
-	return &CompressorHandler{}
+func NewCompressorHandler(s *service.Service) *CompressorHandler {
+	return &CompressorHandler{service: s}
 }
 
 func (s *CompressorHandler) Compress(ctx context.Context, req *proto.CompressRequest) (*proto.CompressResponse, error) {
-
+	videoPath, err := s.service.VideoStorage.VideoById(req.VideoServiceId)
+	if err != nil {
+		return &proto.CompressResponse{Code: 0}, err
+	}
+	fmt.Println(videoPath)
+	nVideo, err := s.service.Compressor.Convert(req, videoPath)
+	if err != nil {
+		return &proto.CompressResponse{Code: 0}, err
+	}
+	fmt.Println(nVideo)
 	return &proto.CompressResponse{Code: 1}, nil
 }
