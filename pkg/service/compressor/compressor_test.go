f45c90c6d6372c04daaac813af5f558f84d72643
Hargeon <hirami14590@gmail.com>
2021-09-30 01:08:01 +0300

    Update compressor service tests

--- pkg/service/compressor/compressor_test.go
+++ pkg/service/compressor/compressor_test.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+const originalVideoPath = "/tmp/original_video/"
+
 func init() {
 	logger := zap.NewExample()
 
@@ -29,134 +31,6 @@ func init() {
 	if ffprobePath == "" {
 		logger.Fatal("Invalid FFMPEG_PATH ENV variable")
 	}
-}
-
-func TestFindBestBitrate(t *testing.T) {
-	cases := []struct {
-		name            string
-		expectedBitrate int
-		bitrateVersions map[int]int
-		expectedIndex   int
-		errorPresent    bool
-	}{
-		{
-			name:            "bitrateVersions is empty",
-			expectedBitrate: 64000,
-			bitrateVersions: map[int]int{},
-			expectedIndex:   -1,
-			errorPresent:    true,
-		},
-		{
-			name:            "one bitrateVersions pair",
-			expectedBitrate: 64000,
-			bitrateVersions: map[int]int{4: 35000},
-			expectedIndex:   4,
-			errorPresent:    false,
-		},
-		{
-			name:            "bitrateVersions less then expected bitrate",
-			expectedBitrate: 64000,
-			bitrateVersions: map[int]int{1: 10000, 2: 63000, 3: 66000},
-			expectedIndex:   2,
-			errorPresent:    false,
-		},
-		{
-			name:            "bitrateVersions biggest then expected bitrate",
-			expectedBitrate: 64000,
-			bitrateVersions: map[int]int{1: 65000, 2: 10000, 3: 38000, 4: 62000},
-			expectedIndex:   1,
-			errorPresent:    false,
-		},
-	}
-
-	for _, testCase := range cases {
-		t.Run(testCase.name, func(t *testing.T) {
-			index, err := findBestBitrate(testCase.expectedBitrate, testCase.bitrateVersions)
-			if index != testCase.expectedIndex {
-				t.Errorf("Invalid Index, expected: %d, got: %d\n", testCase.expectedIndex, index)
-			}
-			if err != nil && !testCase.errorPresent {
-				t.Errorf("Unexpected error %v\n", err)
-			}
-			if err == nil && testCase.errorPresent {
-				t.Errorf("Should be error")
-			}
-		})
-	}
-}
-
-func TestRemoveUselessVideos(t *testing.T) {
-	cases := []struct {
-		name        string
-		videoName   string
-		lastIndex   int
-		escapeIndex int
-		removeCount int
-
-		// creates maxRetryCount files if set true
-		createAllFiles bool
-	}{
-		{
-			name:        "With lastIndex==1, without escapeIndex",
-			videoName:   "test_video.mkv",
-			lastIndex:   1,
-			escapeIndex: -1,
-			removeCount: 1,
-		},
-		{
-			name:        "With lastIndex==20, without escapeIndex",
-			videoName:   "test_video.mkv",
-			lastIndex:   20,
-			escapeIndex: -1,
-			removeCount: 20,
-		},
-		{
-			name:        "With lastIndex==-1, without escapeIndex",
-			videoName:   "test_video.mkv",
-			lastIndex:   -1,
-			escapeIndex: -1,
-			removeCount: 20,
-		},
-		{
-			name:        "With lastIndex==-1, with escapeIndex==5",
-			videoName:   "test_video.mkv",
-			lastIndex:   -1,
-			escapeIndex: 5,
-			removeCount: 19,
-		},
-		{
-			name:        "With lastIndex==6, with escapeIndex==2",
-			videoName:   "test_video.mkv",
-			lastIndex:   6,
-			escapeIndex: 2,
-			removeCount: 5,
-		},
-	}
-
-	root := os.Getenv("ROOT")
-	for _, testCase := range cases {
-		t.Run(testCase.name, func(t *testing.T) {
-			var lastIndex int
-			if testCase.lastIndex == -1 {
-				lastIndex = maxRetryCount
-			} else {
-				lastIndex = testCase.lastIndex
-			}
-			filesPath := createFiles(t, root, testCase.videoName, lastIndex)
-			removeUselessVideos(root, testCase.videoName, testCase.lastIndex, testCase.escapeIndex)
-
-			var filesDeleted int
-			for _, path := range filesPath {
-				if _, err := os.Stat(path); os.IsNotExist(err) {
-					filesDeleted++
-				}
-			}
-			if testCase.removeCount != filesDeleted {
-				t.Errorf("Should remove %d files, removed %d files\n", testCase.removeCount, filesDeleted)
-			}
-		})
-	}
-	clearConvertedVideosDir()
 }
 
 func TestVideoBitrate(t *testing.T) {
@@ -189,7 +63,7 @@ func TestVideoBitrate(t *testing.T) {
 	root := os.Getenv("ROOT")
 	for _, testCase := range cases {
 		t.Run(testCase.name, func(t *testing.T) {
-			path := fmt.Sprintf("%s/tmp/original_video/%s", root, testCase.videoName)
+			path := fmt.Sprintf("%s%s%s", root, originalVideoPath, testCase.videoName)
 			cnf := ffmpeg.Config{
 				FfmpegBinPath:  os.Getenv("FFMPEG_PATH"),
 				FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
@@ -205,7 +79,7 @@ func TestVideoBitrate(t *testing.T) {
 			}
 
 			if err == nil && testCase.errorPresent {
-				t.Errorf("Should be error")
+				t.Errorf("Should be error\n")
 			}
 		})
 	}
@@ -306,18 +180,237 @@ func TestBuildOptions(t *testing.T) {
 	}
 }
 
-func createFiles(t *testing.T, root, videoName string, lastIndex int) []string {
-	filesPath := make([]string, 0, lastIndex)
-	for i := 1; i <= lastIndex; i++ {
-		path := fmt.Sprintf("%s%s/v%d_%s", root, convertedVideosPath, i, videoName)
-		_, err := os.Create(path)
-		if err != nil {
-			t.Errorf("Can't create file for test, error: %s\n", err)
-			continue
-		}
-		filesPath = append(filesPath, path)
+func TestConvertVideo(t *testing.T) {
+	root := os.Getenv("ROOT")
+	originPath := fmt.Sprintf("%s%stest_video.mkv", root, originalVideoPath)
+	newPath := fmt.Sprintf("%s%s/v1_test_video.mkv", root, convertedVideosPath)
+
+	ffmpegCnf := &ffmpeg.Config{
+		FfmpegBinPath:  os.Getenv("FFMPEG_PATH"),
+		FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
 	}
-	return filesPath
+
+	cases := []struct {
+		name      string
+		ffmpegCnf *ffmpeg.Config
+		opts      *ffmpeg.Options
+
+		inputResolution string
+		inputRation     string
+		inputBitrate    string
+		inputBufferSize int
+
+		expectedResolution string
+		expectedRation     string
+
+		errorPresent bool
+		fileCreated  bool
+	}{
+		{
+			name:               "With full ffmpeg config",
+			ffmpegCnf:          ffmpegCnf,
+			inputResolution:    "800:600",
+			inputRation:        "4:3",
+			inputBitrate:       "64000",
+			inputBufferSize:    64000,
+			expectedResolution: "800:600",
+			expectedRation:     "4:3",
+			errorPresent:       false,
+		},
+		{
+			name: "Only with ffprobe path for ffmpeg",
+			ffmpegCnf: &ffmpeg.Config{
+				FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
+			},
+			inputResolution:    "800:600",
+			inputRation:        "4:3",
+			inputBitrate:       "64000",
+			inputBufferSize:    64000,
+			expectedResolution: "",
+			expectedRation:     "",
+			errorPresent:       true,
+		},
+		{
+			name: "Only with ffmpeg path for ffmpeg",
+			ffmpegCnf: &ffmpeg.Config{
+				FfmpegBinPath: os.Getenv("FFMPEG_PATH"),
+			},
+			inputResolution:    "800:600",
+			inputRation:        "4:3",
+			inputBitrate:       "64000",
+			inputBufferSize:    64000,
+			expectedResolution: "",
+			expectedRation:     "",
+			errorPresent:       true,
+		},
+	}
+
+	for i, testCase := range cases {
+		opts := &ffmpeg.Options{
+			Resolution: &testCase.inputResolution,
+			Aspect:     &testCase.inputRation,
+		}
+		testCase.opts = opts
+		cases[i] = testCase
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			defer clearConvertedVideosDir()
+			err := convertVideo(originPath, newPath, testCase.ffmpegCnf, testCase.opts)
+			if err == nil && testCase.errorPresent {
+				t.Errorf("Should be error\n")
+			}
+			if err != nil && !testCase.errorPresent {
+				t.Errorf("Unexpected error: %s\n", err)
+			}
+
+			if testCase.expectedRation != "" || testCase.expectedResolution != "" {
+				metaData, err := ffmpeg.New(ffmpegCnf).Input(newPath).GetMetadata()
+				if err != nil {
+					t.Errorf("Unexpected error while check video metadata, error: %s\n", err)
+					return
+				}
+				streams := metaData.GetStreams()
+				resolution := fmt.Sprintf("%d:%d", streams[0].GetWidth(), streams[0].GetHeight())
+				if resolution != testCase.expectedResolution {
+					t.Errorf("Invalid resolution, expected: %s, got: %s\n", testCase.expectedResolution, resolution)
+				}
+				ratio := streams[0].GetDisplayAspectRatio()
+				if ratio != testCase.expectedRation {
+					t.Errorf("Invalid ration, expected: %s, got: %s\n", testCase.expectedRation, ratio)
+				}
+			}
+		})
+	}
+}
+
+func TestConvertWithBitrate(t *testing.T) {
+	ffmpegCnf := &ffmpeg.Config{
+		FfmpegBinPath:  os.Getenv("FFMPEG_PATH"),
+		FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
+	}
+
+	cases := []struct {
+		name          string
+		originalVideo string
+		inputBuffer   int
+		opts          *ffmpeg.Options
+		ffmpegCnf     *ffmpeg.Config
+
+		expectedBitrate int
+		filePresent     bool
+		errorPresent    bool
+	}{
+		{
+			name:            "Convert test_video.mkv to 64000 bit/s",
+			originalVideo:   "test_video.mkv",
+			ffmpegCnf:       ffmpegCnf,
+			inputBuffer:     64000,
+			expectedBitrate: 64000,
+			filePresent:     true,
+			errorPresent:    false,
+		},
+		{
+			name:            "Convert test_video.mkv to 78000 bit/s",
+			originalVideo:   "test_video.mkv",
+			ffmpegCnf:       ffmpegCnf,
+			inputBuffer:     78000,
+			expectedBitrate: 78000,
+			filePresent:     true,
+			errorPresent:    false,
+		},
+		{
+			name:            "Convert test_video.mkv to 30000 bit/s",
+			originalVideo:   "test_video.mkv",
+			ffmpegCnf:       ffmpegCnf,
+			inputBuffer:     30000,
+			expectedBitrate: 30000,
+			filePresent:     true,
+			errorPresent:    false,
+		},
+		{
+			name:            "Convert bitrate.mkv to 100000 bit/s",
+			originalVideo:   "bitrate.mkv",
+			ffmpegCnf:       ffmpegCnf,
+			inputBuffer:     100000,
+			expectedBitrate: 100000,
+			filePresent:     true,
+			errorPresent:    false,
+		},
+		{
+			name:            "Convert bitrate.mkv to 45000 bit/s",
+			originalVideo:   "bitrate.mkv",
+			ffmpegCnf:       ffmpegCnf,
+			inputBuffer:     45000,
+			expectedBitrate: 45000,
+			filePresent:     true,
+			errorPresent:    false,
+		},
+		{
+			name:          "Convert bitrate.mkv to 45000 bit/s with invalid ffmpeg path",
+			originalVideo: "bitrate.mkv",
+			ffmpegCnf: &ffmpeg.Config{
+				FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
+			},
+			inputBuffer:  45000,
+			filePresent:  false,
+			errorPresent: true,
+		},
+		{
+			name:          "Convert bitrate.mkv to 45000 bit/s with invalid ffrpobe path",
+			originalVideo: "bitrate.mkv",
+			ffmpegCnf: &ffmpeg.Config{
+				FfmpegBinPath: os.Getenv("FFMPEG_PATH"),
+			},
+			inputBuffer:  45000,
+			filePresent:  false,
+			errorPresent: true,
+		},
+	}
+
+	for i, testCase := range cases {
+		buff := testCase.inputBuffer
+		bRate := fmt.Sprintf("%d", testCase.inputBuffer)
+		opts := &ffmpeg.Options{
+			BufferSize:   &buff,
+			VideoBitRate: &bRate,
+		}
+		testCase.opts = opts
+		cases[i] = testCase
+	}
+
+	root := os.Getenv("ROOT")
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			defer clearConvertedVideosDir()
+			originVideoPath := fmt.Sprintf("%s%s%s", root, originalVideoPath, testCase.originalVideo)
+			path, err := convertWithBitrate(originVideoPath, testCase.opts, testCase.ffmpegCnf)
+			if err != nil && !testCase.errorPresent {
+				t.Errorf("Unexpected error, error: %s\n", err)
+			}
+			if err == nil && testCase.errorPresent {
+				t.Errorf("Should be error\n")
+			}
+			if path == "" && testCase.filePresent {
+				t.Errorf("File should be created\n")
+			}
+			if path != "" && !testCase.filePresent {
+				t.Errorf("File should not be created\n")
+			}
+
+			if path != "" {
+				bitrate, err := videoBitrate(path, ffmpegCnf)
+				if err != nil {
+					t.Errorf("Unexpected error while checking video bitrate, err: %s\n", err)
+					return
+				}
+				if bitrate+bitrateAccuracy < testCase.expectedBitrate || bitrate-bitrateAccuracy > testCase.expectedBitrate {
+					t.Errorf("Invalid bitrate, expected %d +- %d, got %d\n", testCase.expectedBitrate, bitrateAccuracy, bitrate)
+				}
+			}
+		})
+	}
 }
 
 func clearConvertedVideosDir() {
@@ -339,5 +432,115 @@ func clearConvertedVideosDir() {
 		if err != nil {
 			logger.Fatal("Can't remove file", zap.String("Error", err.Error()))
 		}
+	}
+}
+
+func TestConvert(t *testing.T) {
+	root := os.Getenv("ROOT")
+	cases := []struct {
+		name          string
+		opt           *proto.CompressRequest
+		originalVideo string
+
+		errorPresent       bool
+		expectedBitrate    int
+		expectedRation     string
+		expectedResolution string
+	}{
+		{
+			name:               "Change resolution test_video.mkv",
+			opt:                &proto.CompressRequest{Resolution: "800:600"},
+			originalVideo:      fmt.Sprintf("%s%stest_video.mkv", root, originalVideoPath),
+			errorPresent:       false,
+			expectedResolution: "800:600",
+		},
+		{
+			name:           "Change ration test_video.mkv",
+			opt:            &proto.CompressRequest{Ratio: "4:3"},
+			originalVideo:  fmt.Sprintf("%s%stest_video.mkv", root, originalVideoPath),
+			errorPresent:   false,
+			expectedRation: "4:3",
+		},
+		{
+			name:            "Change bitrate test_video.mkv",
+			opt:             &proto.CompressRequest{Bitrate: 50000},
+			originalVideo:   fmt.Sprintf("%s%stest_video.mkv", root, originalVideoPath),
+			errorPresent:    false,
+			expectedBitrate: 50000,
+		},
+		{
+			name: "Change bitrate, resolution and ratio test_video.mkv",
+			opt: &proto.CompressRequest{
+				Bitrate:    60000,
+				Resolution: "600:300",
+				Ratio:      "9:6",
+			},
+			originalVideo:      fmt.Sprintf("%s%stest_video.mkv", root, originalVideoPath),
+			errorPresent:       false,
+			expectedBitrate:    60000,
+			expectedRation:     "3:2",
+			expectedResolution: "600:300",
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			defer clearConvertedVideosDir()
+			service := NewCompressor()
+			path, err := service.Convert(testCase.opt, testCase.originalVideo)
+			if err != nil && !testCase.errorPresent {
+				t.Errorf("Unexpected error, error: %s\n", err)
+			}
+			if err == nil && testCase.errorPresent {
+				t.Errorf("Should be error\n")
+			}
+
+			filePresent := testCase.expectedResolution != "" || testCase.expectedRation != "" || testCase.expectedBitrate != 0
+			if filePresent && path == "" {
+				t.Errorf("File should be created\n")
+			}
+
+			if !filePresent && path != "" {
+				t.Errorf("File should not be created\n")
+			}
+
+			if path != "" && filePresent {
+				ffmpegCnf := &ffmpeg.Config{
+					FfmpegBinPath:  os.Getenv("FFMPEG_PATH"),
+					FfprobeBinPath: os.Getenv("FFPROBE_PATH"),
+				}
+
+				if testCase.expectedBitrate != 0 {
+					bitrate, err := videoBitrate(path, ffmpegCnf)
+					if err != nil {
+						t.Errorf("Unexpected error while checking video bitrate, error: %s\n", err)
+					}
+					if (bitrate < testCase.expectedBitrate-bitrateAccuracy) || (bitrate > testCase.expectedBitrate+bitrateAccuracy) {
+						t.Errorf("Invalid bitrate, expected %d +- %d, got %d\n", testCase.expectedBitrate, bitrateAccuracy, bitrate)
+					}
+				}
+
+				metaData, err := ffmpeg.New(ffmpegCnf).Input(path).GetMetadata()
+				if err != nil {
+					t.Errorf("Unexpected error while check video metadata, error: %s\n", err)
+					return
+				}
+				streams := metaData.GetStreams()
+
+				if testCase.expectedResolution != "" {
+					resolution := fmt.Sprintf("%d:%d", streams[0].GetWidth(), streams[0].GetHeight())
+					if resolution != testCase.expectedResolution {
+						t.Errorf("Invalid resolution, expected: %s, got: %s\n", testCase.expectedResolution, resolution)
+					}
+				}
+
+				if testCase.expectedRation != "" {
+					ratio := streams[0].GetDisplayAspectRatio()
+					if ratio != testCase.expectedRation {
+						t.Errorf("Invalid ration, expected: %s, got: %s\n", testCase.expectedRation, ratio)
+					}
+				}
+			}
+		})
 	}
 }
