e6e60db4c5dc9c8e73eb36375cba92f9c94efbca
Hargeon <hirami14590@gmail.com>
2021-11-23 02:15:19 +0300

    Add compress handler

--- pkg/handler/compressor.go
+++ pkg/handler/compressor.go
@@ -1,30 +1,121 @@
+// Package handler uses for downloading original video, converting file and uploading converted video
 package handler
 
 import (
 	"context"
-	"fmt"
-	"github.com/Hargeon/compressrv/pkg/proto"
+	"os"
+
+	"github.com/Hargeon/compressrv/pkg/response"
 	"github.com/Hargeon/compressrv/pkg/service"
+	"github.com/Hargeon/compressrv/pkg/service/compressor"
+
+	"go.uber.org/zap"
 )
 
+// CompressorHandler uses for compressing video file
 type CompressorHandler struct {
-	service *service.Service
+	srv    *service.Service
+	logger *zap.Logger
 }
 
-func NewCompressorHandler(s *service.Service) *CompressorHandler {
-	return &CompressorHandler{service: s}
+// NewHandler initialize CompressorHandler
+func NewHandler(s *service.Service, logger *zap.Logger) *CompressorHandler {
+	return &CompressorHandler{srv: s, logger: logger}
 }
 
-func (s *CompressorHandler) Compress(ctx context.Context, req *proto.CompressRequest) (*proto.CompressResponse, error) {
-	videoPath, err := s.service.VideoStorage.VideoById(req.VideoServiceId)
+// Compress video file and build response
+func (h *CompressorHandler) Compress(ctx context.Context, req *compressor.Request) *response.Response {
+	resp := &response.Response{RequestID: req.RequestID}
+	videoName, err := h.srv.Download(ctx, req.VideoServiceID)
+
 	if err != nil {
-		return &proto.CompressResponse{Code: 0}, err
+		h.logger.Error("Download original video",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+
+		resp.Error = "Can't download original video from cloud"
+
+		return resp
 	}
-	fmt.Println(videoPath)
-	nVideo, err := s.service.Compressor.Convert(req, videoPath)
+
+	defer func() {
+		os.Remove(videoName)
+	}()
+
+	fileInfo, err := h.srv.VideoInfo(videoName)
+	if err == nil {
+		resp.OriginalVideo = &response.OriginalVideo{
+			ID:    req.VideoID,
+			Video: *fileInfo,
+		}
+	} else {
+		h.logger.Error("original video video info",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+	}
+
+	convertedVideoPath, err := h.srv.Convert(ctx, req, videoName)
 	if err != nil {
-		return &proto.CompressResponse{Code: 0}, err
+		h.logger.Error("Convert original video",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+
+		resp.Error = "Error occurred when converting video"
+
+		return resp
 	}
-	fmt.Println(nVideo)
-	return &proto.CompressResponse{Code: 1}, nil
+
+	defer func() {
+		os.Remove(convertedVideoPath)
+	}()
+
+	convertedVideo, err := os.Open(convertedVideoPath)
+	if err != nil {
+		h.logger.Error("open converted video",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+
+		resp.Error = "error occurred when reading converted video"
+
+		return resp
+	}
+
+	id, err := h.srv.Upload(ctx, req.VideoServiceID, convertedVideo)
+	if err != nil {
+		h.logger.Error("upload converted video",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+
+		resp.Error = "error occurred when uploading converted video"
+
+		return resp
+	}
+
+	fileInfo, err = h.srv.VideoInfo(convertedVideoPath)
+	if err != nil {
+		h.logger.Error("converted video video info",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+
+		resp.Error = "error occurred when getting stats converted video"
+
+		return resp
+	}
+
+	resp.ConvertedVideo = &response.ConvertedVideo{
+		ServiceID: id,
+		Video:     *fileInfo,
+		Name:      id,
+	}
+
+	stat, err := convertedVideo.Stat()
+	if err == nil {
+		resp.ConvertedVideo.Size = stat.Size()
+	} else {
+		h.logger.Error("error occurred when getting size of converted video",
+			zap.String("Error", err.Error()),
+			zap.Int64("VideoID", req.VideoID))
+	}
+
+	return resp
 }
--- pkg/handler/compressor_test.go
+++ pkg/handler/compressor_test.go
@@ -2,17 +2,20 @@ package handler
 
 import (
 	"context"
-	"github.com/Hargeon/compressrv/pkg/proto"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/Hargeon/compressrv/pkg/response"
 	"github.com/Hargeon/compressrv/pkg/service"
-	"github.com/Hargeon/compressrv/pkg/service/storage"
+	"github.com/Hargeon/compressrv/pkg/service/compressor"
+
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
-	"log"
-	"net"
-	"os"
-	"testing"
 )
 
 func init() {
@@ -25,62 +28,230 @@ func init() {
 	if err != nil {
 		logger.Fatal("Can't read .env file", zap.String("Error", err.Error()))
 	}
-	root := os.Getenv("ROOT")
-	if root == "" {
+
+	if root := os.Getenv("ROOT"); root == "" {
 		logger.Fatal("Invalid ROOT ENV variable")
 	}
 }
 
-func dialer() func(context.Context, string) (net.Conn, error) {
-	listener := bufconn.Listen(1024 * 1024)
-	server := grpc.NewServer()
+type errorCloud struct{}
 
-	logger := zap.NewExample()
-	localStorage := storage.NewLocalStorage(logger)
-	srv := service.NewService(localStorage)
-	handler := NewCompressorHandler(srv)
-	proto.RegisterCompressorServer(server, handler)
+func (s *errorCloud) Download(ctx context.Context, id string) (string, error) {
+	return "", errors.New("mock failed")
+}
 
-	go func() {
-		if err := server.Serve(listener); err != nil {
-			log.Fatalln(err)
-		}
-	}()
+func (s *errorCloud) Upload(ctx context.Context, fileName string, file io.Reader) (string, error) {
+	return "", errors.New("mock failed")
+}
 
-	return func(context.Context, string) (net.Conn, error) {
-		return listener.Dial()
+type successCloud struct{}
+
+func (s *successCloud) Download(ctx context.Context, id string) (string, error) {
+	src := fmt.Sprintf("%s/tmp/original_video/test_video.mkv", os.Getenv("ROOT"))
+	dst := fmt.Sprintf("%s/tmp/converted_video/temp_original_file.mkv", os.Getenv("ROOT"))
+	sourceFileStat, err := os.Stat(src)
+
+	if err != nil {
+		return "", err
 	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return "", fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return "", err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return "", err
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+
+	if err != nil {
+		return "", err
+	}
+
+	return dst, nil
+}
+
+func (s *successCloud) Upload(ctx context.Context, fileName string, file io.Reader) (string, error) {
+	return "temp_converted_file.mkv", nil
+}
+
+type errorCompressService struct{}
+
+func (e *errorCompressService) Convert(ctx context.Context, opt *compressor.Request, originalVideo string) (string, error) {
+	return "", errors.New("failed mock file convert")
+}
+
+func (e *errorCompressService) VideoInfo(path string) (*response.Video, error) {
+	return nil, errors.New("failed mock file info")
+}
+
+type successCompressService struct{}
+
+func (s *successCompressService) Convert(ctx context.Context, opt *compressor.Request, originalVideo string) (string, error) {
+	src := fmt.Sprintf("%s/tmp/original_video/bitrate.mkv", os.Getenv("ROOT"))
+	dst := fmt.Sprintf("%s/tmp/converted_video/temp_converted_file.mkv", os.Getenv("ROOT"))
+	sourceFileStat, err := os.Stat(src)
+
+	if err != nil {
+		return "", err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return "", fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return "", err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return "", err
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		return "", err
+	}
+
+	return dst, nil
+}
+
+func (s *successCompressService) VideoInfo(path string) (*response.Video, error) {
+	resp := &response.Video{
+		Bitrate:     64000,
+		ResolutionX: 800,
+		ResolutionY: 600,
+		RatioX:      4,
+		RatioY:      3,
+	}
+
+	return resp, nil
 }
 
 func TestCompress(t *testing.T) {
+	logger := zap.NewExample()
+
 	cases := []struct {
-		name string
-		req  *proto.CompressRequest
+		name             string
+		srv              *service.Service
+		req              *compressor.Request
+		expectedResponse *response.Response
 	}{
 		{
-			name: "Check",
-			req: &proto.CompressRequest{
+			name: "Invalid downloading video",
+			srv: &service.Service{
+				VideoStorage: &errorCloud{},
+			},
+			req: &compressor.Request{
+				RequestID:      1,
 				Bitrate:        64000,
 				Resolution:     "800:600",
 				Ratio:          "4:3",
-				VideoServiceId: "test_video.mkv",
+				VideoID:        1,
+				VideoServiceID: "mock_service",
+			},
+			expectedResponse: &response.Response{
+				RequestID: 1,
+				Error:     "Can't download original video from cloud",
+			},
+		},
+		{
+			name: "Invalid converting video",
+			srv: &service.Service{
+				VideoStorage: &successCloud{},
+				Compressor:   &errorCompressService{},
+			},
+			req: &compressor.Request{
+				RequestID:      1,
+				Bitrate:        64000,
+				Resolution:     "800:600",
+				Ratio:          "4:3",
+				VideoID:        1,
+				VideoServiceID: "mock_service",
+			},
+			expectedResponse: &response.Response{
+				RequestID: 1,
+				Error:     "Error occurred when converting video",
+			},
+		},
+		{
+			name: "Valid converting video",
+			srv: &service.Service{
+				VideoStorage: &successCloud{},
+				Compressor:   &successCompressService{},
+			},
+			req: &compressor.Request{
+				RequestID:      1,
+				Bitrate:        64000,
+				Resolution:     "800:600",
+				Ratio:          "4:3",
+				VideoID:        1,
+				VideoServiceID: "mock_service",
+			},
+			expectedResponse: &response.Response{
+				RequestID: 1,
+				OriginalVideo: &response.OriginalVideo{
+					ID: 1,
+					Video: response.Video{
+						Bitrate:     64000,
+						ResolutionX: 800,
+						ResolutionY: 600,
+						RatioX:      4,
+						RatioY:      3,
+					},
+				},
+				ConvertedVideo: &response.ConvertedVideo{
+					ServiceID: "temp_converted_file.mkv",
+					Size:      3595197,
+					Name:      "temp_converted_file.mkv",
+					Video: response.Video{
+						Bitrate:     64000,
+						ResolutionX: 800,
+						ResolutionY: 600,
+						RatioX:      4,
+						RatioY:      3,
+					},
+				},
 			},
 		},
 	}
 
-	ctx := context.Background()
-
-	conn, err := grpc.DialContext(ctx, "", grpc.WithInsecure(), grpc.WithContextDialer(dialer()))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer conn.Close()
-	client := proto.NewCompressorClient(conn)
-
 	for _, testCase := range cases {
 		t.Run(testCase.name, func(t *testing.T) {
-			client.Compress(ctx, testCase.req)
-			//if err
+			srv := NewHandler(testCase.srv, logger)
+
+			resp := srv.Compress(context.Background(), testCase.req)
+
+			if !reflect.DeepEqual(resp, testCase.expectedResponse) {
+				t.Errorf("Expected original video: %v, got: %v\n",
+					testCase.expectedResponse.OriginalVideo, resp.OriginalVideo)
+
+				t.Errorf("Expected converted video: %v, got: %v\n",
+					testCase.expectedResponse.ConvertedVideo, resp.ConvertedVideo)
+
+				if testCase.expectedResponse.RequestID != resp.RequestID {
+					t.Errorf("Expected request id: %d, got: %d\n",
+						testCase.expectedResponse.RequestID, resp.RequestID)
+				}
+
+				if testCase.expectedResponse.Error != resp.Error {
+					t.Errorf("Expected error: %s, got: %s\n",
+						testCase.expectedResponse.Error, resp.Error)
+				}
+			}
 		})
 	}
 }
